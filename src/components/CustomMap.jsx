a453ce4d360dfbad8bd9abdf275dff6aa30bdb4f
swati-salunke <149562665+swati-salunke@users.noreply.github.com>
2024-01-08 18:10:43 +0530

    one more update

---
+++ src/components/CustomMap.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CustomMap = () => {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1890.7849305925902!2d73.81909880500079!3d18.593421223636675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b8bfafe5437d%3A0xece5683a32fb6491!2sBhairavnath%20Temple!5e0!3m2!1sen!2sin!4v1704701405897!5m2!1sen!2sin"
+      width="600"
+      height="450"
+      style={{ border: '0' }}
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
+  );
+};
+
+export default CustomMap;
